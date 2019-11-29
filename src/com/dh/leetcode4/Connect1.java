e211a0dbc1d5186919dac2c5d07364aa0a2fd065
Lenovo <757486169@qq.com>
2019-11-29 10:13:34 +0800

    111

--- src/com/dh/leetcode4/Connect1.java
+++ src/com/dh/leetcode4/Connect1.java
@@ -24,17 +24,18 @@ public class Connect1 {
 		if (node.left != null && node.right == null)
 			temp = node.left;
 		while (temp1.next != null) {
+			System.out.println(temp1.val);
 			if (temp1.next.left != null && temp1.next.right != null) {
+				
 				temp.next = temp1.next.left;
-				temp1.next.left = temp1.next.right;
+				temp1.next.left.next = temp1.next.right;
+				
 				temp = temp1.next.right;
-			}
-			if (temp1.next.left == null && temp1.next.right != null) {
-				temp1.next = temp1.next.right;
-				temp1 = temp1.next.right;
-			}
-			if (temp1.next.left != null && temp1.next.right == null) {
-				temp1.next = temp1.next.left;
+			} else if (temp1.next.left == null && temp1.next.right != null) {
+				temp.next = temp1.next.right;
+				temp = temp1.next.right;
+			} else if (temp1.next.left != null && temp1.next.right == null) {
+				temp.next = temp1.next.left;
 				temp = temp1.next.left;
 			}
 			temp1 = temp1.next;
